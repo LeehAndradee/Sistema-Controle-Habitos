55f48b89543563087e0dea3636b78ade2931d214
Leticia Andrade <leticiaandradeoliveira@outlook.com>
2025-11-21 14:00:56 +0000

    Criação das Models

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DailyCheck(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    date = models.DateField()
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.habit.name} - {self.date}"
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def habit_list(request):
+    return render(request, 'habits/habit_list.html')
