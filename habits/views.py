21738ca9a52ddf089e34ba6e9d93371d62ea527d
Leticia Andrade <leticiaandradeoliveira@outlook.com>
2025-11-22 17:50:59 +0000

    Projeto Inicial Funcionando

--- habits/views.py
+++ habits/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Habit
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def habit_list(request):
-    return render(request, 'habits/habit_list.html')
+    habits = Habit.objects.filter(user=request.user)
+    return render(request, 'habits/habit_list.html', {'habits': habits})
